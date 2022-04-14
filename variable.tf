5afa946c7f2b25888abc4e79b42974ee532f4a7e
tuminbangkok <attapol26@gmail.com>
2022-04-14 13:48:53 +0700

    add prefix in main

---
+++ variable.tf
@@ -0,0 +1,8 @@
+variable "prefix" {
+  default     = "rg"
+  description = "The prefix which should be used for all resources in this example"
+}
+variable "location" {
+  default     = "southeast asia"
+  description = "The Azure Region in which all resources in this example should be created."
+}
